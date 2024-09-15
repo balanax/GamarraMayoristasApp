5eaa46adfbffb00f841e8932e069748e848734eb
Danny Herrera <danielherrera27@outlook.com>
2024-09-14 19:19:39 -0500

    Primera Version App
    Primera version de GamarraMayoristasApp

---
+++ app/src/main/java/com/upc/gamarramayoristasapp/carrito/CarritoVerificar.kt
@@ -0,0 +1,21 @@
+package com.upc.gamarramayoristasapp.carrito
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.upc.gamarramayoristasapp.R
+
+class CarritoVerificar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_carrito_verificar)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/upc/gamarramayoristasapp/pago/DireccionEnvio.kt
@@ -0,0 +1,21 @@
+package com.upc.gamarramayoristasapp.pago
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.upc.gamarramayoristasapp.R
+
+class DireccionEnvio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_direccion_envio)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
