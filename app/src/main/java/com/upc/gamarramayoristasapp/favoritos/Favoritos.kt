f33235aea87dc239b7f384ff76eb805173625ccc
mmatosupcweb <matos114@gmail.com>
2024-09-16 18:25:21 -0500

    actualizar
    vistas

---
+++ app/src/main/java/com/upc/gamarramayoristasapp/favoritos/Favoritos.kt
@@ -0,0 +1,27 @@
+package com.upc.gamarramayoristasapp.favoritos
+
+import android.os.Bundle
+import android.view.Menu
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.upc.gamarramayoristasapp.R
+
+class Favoritos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_favoritos)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_desplegable,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+}
