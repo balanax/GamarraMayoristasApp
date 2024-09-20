10175f9cdcd30a509832ba6648f855d0850df6f3
ElHuGhXD <elhughxd@gmail.com>
2024-09-19 23:59:58 -0500

    Avance Ordenes con datos

--- app/src/main/java/com/upc/gamarramayoristasapp/perfil/OrdenesModel.kt
+++ app/src/main/java/com/upc/gamarramayoristasapp/perfil/OrdenesModel.kt
@@ -1,11 +1,11 @@
 package com.upc.gamarramayoristasapp.perfil
 
-data class Orden(
+data class OrdenesModel (
     val id: String,
     val nrorden: String,
     val fecha: String,
     val nrotraking: String,
-    val cantidad: Int,
-    val monto: Double,
+    val cantidad: String,
+    val monto: String,
     val estado: String
 )
