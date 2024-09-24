4245ce69008cec89479fbea0eda54b47441c1b9d
Danny Herrera <danielherrera27@outlook.com>
2024-09-23 22:26:55 -0500

    Programacion del menu

--- app/src/main/java/com/upc/gamarramayoristasapp/carrito/Carrito.kt
+++ app/src/main/java/com/upc/gamarramayoristasapp/carrito/Carrito.kt
@@ -1,5 +1,6 @@
 package com.upc.gamarramayoristasapp.carrito
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,8 +8,13 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.upc.gamarramayoristasapp.R
 import com.upc.gamarramayoristasapp.carrito.adapter.CarritoAdapter
+import com.upc.gamarramayoristasapp.favoritos.Favoritos
+import com.upc.gamarramayoristasapp.inicio.Inicio
+import com.upc.gamarramayoristasapp.perfil.Perfil
+import com.upc.gamarramayoristasapp.producto.Categoria
 
 class Carrito : AppCompatActivity() {
 
@@ -22,6 +28,44 @@ class Carrito : AppCompatActivity() {
             insets
         }
         initRecycleView()
+
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottomNavigationView.selectedItemId = R.id.menu_carrito
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.menu_inicio -> {
+                    // Acción para Home
+                    var intent = Intent(this, Inicio::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.menu_carrito -> {
+                    var intent = Intent(this,Carrito::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.menu_categoria -> {
+                    var intent = Intent(this, Categoria::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.menu_favoritos -> {
+                    var intent = Intent(this, Favoritos::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.menu_perfil -> {
+                    var intent = Intent(this, Perfil::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
+
     }
 
     private fun initRecycleView(){
--- app/src/main/java/com/upc/gamarramayoristasapp/inicio/Inicio.kt
+++ app/src/main/java/com/upc/gamarramayoristasapp/inicio/Inicio.kt
@@ -3,13 +3,19 @@ package com.upc.gamarramayoristasapp.inicio
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.upc.gamarramayoristasapp.R
 import com.upc.gamarramayoristasapp.carrito.Carrito
+import com.upc.gamarramayoristasapp.favoritos.Favoritos
+import com.upc.gamarramayoristasapp.perfil.Perfil
+import com.upc.gamarramayoristasapp.producto.Categoria
 
 class Inicio : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,14 +31,48 @@ class Inicio : AppCompatActivity() {
             insets
         }
 
-        val btnCarrito = findViewById<Button>(R.id.btnCarrito)
+//        val btnCarrito = findViewById<Button>(R.id.btnCarrito)
+//
+//        btnCarrito.setOnClickListener {
+//            var intent = Intent(this, Carrito::class.java)
+//            startActivity(intent)
+//        }
 
-        btnCarrito.setOnClickListener {
-            var intent = Intent(this, Carrito::class.java)
-            startActivity(intent)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.menu_inicio -> {
+                    // Acción para Home
+                    var intent = Intent(this,Inicio::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.menu_carrito -> {
+                    var intent = Intent(this,Carrito::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.menu_categoria -> {
+                    var intent = Intent(this,Categoria::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.menu_favoritos -> {
+                    var intent = Intent(this, Favoritos::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.menu_perfil -> {
+                    var intent = Intent(this, Perfil::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
         }
+
+
     }
-
-
 
 }
