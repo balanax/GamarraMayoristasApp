715d24048291d10bcbf5de9e73f4f551e3fed25d
ElHuGhXD <elhughxd@gmail.com>
2024-10-02 00:07:05 -0500

    Ultimos ajustes del Detalle de Orden

--- app/src/main/java/com/upc/gamarramayoristasapp/pago/DetalleOrden.kt
+++ app/src/main/java/com/upc/gamarramayoristasapp/pago/DetalleOrden.kt
@@ -6,9 +6,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.upc.gamarramayoristasapp.R
 
 class DetalleOrden : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,6 +23,10 @@ class DetalleOrden : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        recyclerView = findViewById(R.id.rvDetalleOrdenes)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
         val nroOrden = intent.getStringExtra("nroOrden")
         val fecha = intent.getStringExtra("fecha")
@@ -30,6 +39,7 @@ class DetalleOrden : AppCompatActivity() {
         findViewById<TextView>(R.id.txtFecha).text = "${fecha}"
         findViewById<TextView>(R.id.txtTraking).text = "Nro. Traking: ${nrotraking}"
         findViewById<TextView>(R.id.txtEstado).text = "${estado}"
+        findViewById<TextView>(R.id.txtCantidad).text = "${cantidad} items"
 
     }
 }
