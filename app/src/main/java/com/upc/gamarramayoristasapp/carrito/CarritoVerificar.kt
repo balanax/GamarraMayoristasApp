1de655ff3aebe4d3774ccbcfcc61dc865980f9e5
Branny Ruiz <61120777+brannyruiz@users.noreply.github.com>
2024-10-04 09:31:13 -0500

    Actualizacion cambio de dirección

--- app/src/main/java/com/upc/gamarramayoristasapp/carrito/CarritoVerificar.kt
+++ app/src/main/java/com/upc/gamarramayoristasapp/carrito/CarritoVerificar.kt
@@ -7,11 +7,14 @@ import android.os.Parcelable
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.upc.gamarramayoristasapp.R
+import com.upc.gamarramayoristasapp.pago.DireccionEnvio
+
 
 class CarritoVerificar() : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +26,21 @@ class CarritoVerificar() : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
 
+        val bt_siguiente = findViewById<ImageButton>(R.id.ivRetroceder)
+        bt_siguiente.setOnClickListener{
+            var intent = Intent(this,Carrito::class.java)
+            startActivity(intent)
+        }
+
+        val bt_cambiar = findViewById<TextView>(R.id.tvCambiar)
+        bt_cambiar.setOnClickListener{
+            var intent = Intent(this, DireccionEnvio::class.java)
+            startActivity(intent)
+        }
+
+
+
+    }
 
 }
